4de48c27e68cbdd48f28ed4e85b817c61441294c
Mikhail Maliar <mike@drupfan.com>
2022-10-02 13:26:58 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useContext, useEffect} from "react";
+import {StoreLocatorContext} from "./context/StoreLocatorContext";
+import Block from "./components/Block";
+import Block2 from "./components/Block2";
+import Input from "./components/Input";
 
 function App() {
+  const { storeLocatorState, setQuery } = useContext(StoreLocatorContext);
+  console.log({storeLocatorState})
+  useEffect(() => {
+    setQuery('query')
+  }, [])
+
+  const styles = {
+    block1: {
+      width: "300px",
+      height: "400px",
+      position: "absolute",
+      background: "red",
+      top: 0,
+      left: 0
+    },
+    block2: {
+      width: "500px",
+      height: "600p",
+      position: "absolute",
+      background: "black",
+      bottom: 0,
+      right: 0
+    },
+    input: {
+      color: "blue"
+    }
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<Block style={styles.block1}/>*/}
+      {/*<Button color='blue'/>*/}
+      <div style={{display: 'grid', gridTemplateColumns: '27fr 100fr', border: '1px solid #000', columnGap: '20px',   wordBreak: 'break-all'}}>
+        <div>dedweddwefwefwefwefwefwefwfwefwefwefwefwefwefdedweddwefwefwefwefwefwefwfwefwefwefwefwefwefdedweddwefwefwefwefwefwefwfwefwefwefwefwefwef</div>
+        <div style={{gridColumnStart: 2}}>dedweddwefwefwefwefwefwefwfwefwefwefwefwefwefdedweddwefwefwefwefwefwefwfwefwefwefwefwefwefdedweddwefwefwefwefwefwefwfwefwefwefwefwefwefdedweddwefwefwefwefwefwefwfwefwefwefwefwefwef</div>
+        <div style={{gridColumnStart: 2, gridRowStart: 1}}>edededew</div>
+      </div>
+      {/*<Block2 style={styles.block2}/>*/}
+      {/*<Input style={styles.input}/>*/}
+      {/*<StoreLocator />*/}
     </div>
   );
 }
